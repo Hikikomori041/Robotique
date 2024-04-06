ba62193b98f0fccf114238606f514c4d2a439107
BLACHERE Nicolas <nicolas.blachere6@etu.univ-lorraine.fr>
2024-04-06 03:01:10 +0200

    on teste des trucs

--- programme.py
+++ programme.py
@@ -10,25 +10,42 @@ from ev3dev2.sensor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2.sensor.virtual import * # utilisé par le simulateur, à commenter pour une exécution réelle
 
-# Create the sensors and motors objects
-roueGauche = OUTPUT_B
-roueDroite = OUTPUT_C
+
+# Les 4 directions possibles du robot
+class Direction(object):
+    HAUT = 1
+    DROITE = 2
+    BAS = 3
+    GAUCHE = 4
+
+
+# Inputs et outputs du robot
+spkr = Sound()
+btn = Button()
+
+
+# Moteurs
+roueGauche = OUTPUT_A # A dans le simulateur, B dans le réel
+roueDroite = OUTPUT_B # B dans le simulateur, C dans le réel
 left_motor = LargeMotor(roueGauche)
 right_motor = LargeMotor(roueDroite)
 tank_drive = MoveTank(roueGauche, roueDroite)
 steering_drive = MoveSteering(roueGauche, roueDroite)
 
-spkr = Sound()
-btn = Button()
 
+# Capteurs
 ultrasonic_sensor = UltrasonicSensor(INPUT_4)
 touch_sensor = TouchSensor(INPUT_1)
 
 
-# Here is where your code starts
+
+#-------------------------------------------
+#               Programme
+#-------------------------------------------
 diametreRoue = 5.6 #cm
 circonferenceRoue = round(diametreRoue * math.pi, 2)
 
+direction = Direction.HAUT
 robot_x = None
 robot_y = None
 coords = None
@@ -45,8 +62,12 @@ while not touch_sensor.is_pressed:
     robot_y += round(circonferenceRoue, 2)
     tank_drive.on(SpeedRPS(1), SpeedRPS(1))
     if distance < 255:
-        x = round(robot_x + distance, 2)
-        y = round(robot_y, 2)
+        # Le capteur US a détecté quelque chose sur la droite du robot
+        if(direction == Direction.HAUT):
+            x = round(robot_x + distance, 2)
+            y = round(robot_y, 2)
+        else:
+            print("DIRECTION NON GÉRÉE: " + str(direction))
         obstacle = [x, y]
         print('Obstacle: ' + str(x) + ', ' + str(y))
         coords.append(obstacle)
