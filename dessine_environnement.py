1801bdda90b4560d64aa3dd290fe5069f86023ae
BLACHERE Nicolas <nicolas.blachere6@etu.univ-lorraine.fr>
2024-04-06 05:19:39 +0200

    ça commence à fonctionner

---
+++ dessine_environnement.py
@@ -0,0 +1,109 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # Hide the hello message from pygame
+import pygame
+import sys
+
+import random
+
+# Fonctions et classes
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def draw_line(color, point1, point2):
+    pygame.draw.line(window_surface, color, (point1.x, point1.y), (point2.x, point2.y), 4) # épaisseur du trait de 3 pixels
+
+def draw_line_from_center(color, point1, point2):
+   draw_line(color, Point( round(point1.x+CENTER,2) , round(point1.y+CENTER,2) ), Point( round(point2.x+CENTER,2) , round(point2.y+CENTER,2) ))
+
+def drawCrossAt0_0():
+    # Define the cross
+    cross_color = (255, 255, 255)  # White color
+    cross_thickness = 1
+    cross_length = 10  # Length of the cross lines
+
+    # Draw the cross
+    pygame.draw.line(window_surface, cross_color, (CENTER - cross_length, CENTER), (CENTER + cross_length, CENTER), cross_thickness)
+    pygame.draw.line(window_surface, cross_color, (CENTER, CENTER - cross_length), (CENTER, CENTER + cross_length), cross_thickness)
+
+def recupererListeCoordonnees():
+    global nb_coords, continueLigne
+    nb_coords = 0
+    liste = []
+
+    # Ouvrir le fichier en mode lecture
+    with open('environnement.txt', 'r') as file:
+        for line in file:
+            # Supprimer les sauts de ligne
+            line = line.strip()
+            if line != "":
+                if line == "_":
+                    continueLigne = False
+                    print("On arrête la ligne")
+                else:
+                    # La ligne représente les coordonnées d'un obstacle
+                    # Séparer les coordonnées
+                    x, y = line.split(',')
+                    
+                    # Convertir les coordonnées en entiers et les ajouter à la liste
+                    point = Point(int(float(x)), -int(float(y)))
+                    liste.append(point)
+                    nb_coords += 1
+    return liste
+
+# Définition des constantes
+SIZE = 800 # 800cm donc 8 mètres
+CENTER = SIZE/2
+MAX_COORDS = CENTER - 10
+
+# Création de la fenêtre
+pygame.init()
+window_resolution=(SIZE,SIZE)
+window_surface=pygame.display.set_mode(window_resolution)
+pygame.display.set_caption('Drawing walls')
+
+# Configuration de l'intervalle de temps (500 ms)
+intervalle = 50  # en millisecondes
+
+# Création d'un événement personnalisé pour appeler la fonction
+MON_EVENEMENT = pygame.USEREVENT + 1
+pygame.time.set_timer(MON_EVENEMENT, intervalle)
+
+# Compteur pour suivre le temps écoulé
+temps_ecoule = 0
+i = 0
+dernieresCoordonnees = None
+listeCoordonnees = recupererListeCoordonnees()
+continueLigne = True
+
+# Durée totale d'exécution
+duree_totale = intervalle * nb_coords  # en millisecondes
+
+drawCrossAt0_0()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
+        elif event.type == MON_EVENEMENT and temps_ecoule < duree_totale:
+            coordonnees = listeCoordonnees[i]
+            if (dernieresCoordonnees != None):
+                if i%2==0:
+                    color = (192,192,192)
+                else:
+                    color = (144,144,144)
+                # color = random.choices(range(256), k=3)
+                if not continueLigne:
+                    dernieresCoordonnees = coordonnees
+                    continueLigne = True
+                draw_line_from_center(color, dernieresCoordonnees, coordonnees)
+            dernieresCoordonnees = coordonnees
+            i += 1
+            temps_ecoule += intervalle
+            pygame.display.flip()
+
+# Nettoyage et fermeture de Pygame
+pygame.quit()
--- programme.py
+++ programme.py
@@ -10,7 +10,6 @@ from ev3dev2.sensor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2.sensor.virtual import * # utilisé par le simulateur, à commenter pour une exécution réelle
 
-
 # Les 4 directions possibles du robot
 class Direction(object):
     HAUT = 1
@@ -37,40 +36,185 @@ steering_drive = MoveSteering(roueGauche, roueDroite)
 ultrasonic_sensor = UltrasonicSensor(INPUT_4)
 touch_sensor = TouchSensor(INPUT_1)
 
+#-------------------------------------------
+# Fonctions
+def changeDirection(cote):
+    if (cote == "gauche"):
+        if direction == Direction.HAUT:
+            return Direction.GAUCHE
+        elif direction == Direction.DROITE:
+            return Direction.HAUT
+        elif direction == Direction.BAS:
+            return Direction.DROITE
+        elif direction == Direction.GAUCHE:
+            return Direction.BAS
+    else:
+        if direction == Direction.HAUT:
+            return Direction.DROITE
+        elif direction == Direction.DROITE:
+            return Direction.BAS
+        elif direction == Direction.BAS:
+            return Direction.GAUCHE
+        elif direction == Direction.GAUCHE:
+            return Direction.HAUT
+    return direction
 
+
+def tourne(cote = "droite"):
+    global direction # récupère la variable globale direction
+    temps = 1550 / 1000
+
+    if (DEBUG):
+        print("Tourne à " + cote)
+
+    if (cote == "gauche"):
+        # On tourne à gauche
+        steering_drive.on_for_seconds(-90, SpeedRPS(0.5), temps)
+    else:
+        # On tourne à droite
+        steering_drive.on_for_seconds(90, SpeedRPS(0.5), temps)
+    tank_drive.off(brake=True)
+    direction = changeDirection(cote)
+    return
+
+def recule():
+    global robot_x, robot_y # On récupère les coordonnées du robot du programme
+
+    if (DEBUG):
+        print("Le robot recule")
+    # S'arrête
+    tank_drive.off(brake=True)
+    spkr.play_tone(800, 0.1, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
+    time.sleep(500/1000)
+    # Recule
+    tank_drive.on_for_seconds(SpeedRPS(1) * -1, SpeedRPS(1) * -1, 1)
+    time.sleep(500/1000)
+
+    # Calcul les nouvelles coordonnées du robot
+    deplacement = circonferenceRoue
+    if(direction == Direction.HAUT):
+        robot_y -= deplacement
+    elif(direction == Direction.BAS):
+        robot_y += deplacement
+    elif(direction == Direction.DROITE):
+        robot_x -= deplacement
+    elif(direction == Direction.GAUCHE):
+        robot_x += deplacement
+
+    #todo: verifier dans quelle direction on doit tourner
+    tourne("gauche")
+    return
+
+def avance():
+    global robot_x, robot_y # On récupère les coordonnées du robot du programme
+    global coords # On utilise la liste en variable globale pour pouvoir l'affecter
+    global detecteUnObstacle
+
+    tank_drive.on(SpeedRPS(1), SpeedRPS(1))
+    deplacement = circonferenceRoue/30 # On part du principe que le calcul se fait 30 fois par seconde
+    
+    if(direction == Direction.HAUT):
+        robot_y += deplacement
+    elif(direction == Direction.BAS):
+        robot_y -= deplacement
+    elif(direction == Direction.GAUCHE):
+        robot_x -= deplacement
+    elif(direction == Direction.DROITE):
+        robot_x += deplacement
+    
+    distance = ultrasonic_sensor.distance_centimeters
+    if distance < 255:
+        # Le capteur US a détecté quelque chose sur la droite du robot
+        detecteUnObstacle = True
+        if(direction == Direction.HAUT):
+            x = round(robot_x + distance, 2)
+            y = round(robot_y, 2)
+        elif(direction == Direction.DROITE):
+            x = round(robot_x, 2)
+            y = round(robot_y - distance, 2)
+        elif(direction == Direction.BAS):
+            x = round(robot_x - distance, 2)
+            y = round(robot_y, 2)
+        elif(direction == Direction.GAUCHE):
+            x = round(robot_x, 2)
+            y = round(robot_y + distance, 2)
+        else:
+            if (DEBUG):
+                print("DIRECTION NON GÉRÉE: " + str(direction))
+            exit()
+        if DEBUG:
+            print('Obstacle: ' + str(x) + ', ' + str(y))
+        coords += str(x) + ',' + str(y) + "\n"
+    else:
+        if detecteUnObstacle:
+            # Ici, on ne détecte plus un obstacle, on devrait donc pouvoir tourner
+            detecteUnObstacle = False
+            coords += "_\n"
+            time.sleep(500/1000)
+            tourne("droite")
 
 #-------------------------------------------
 #               Programme
 #-------------------------------------------
+
+# Instanciation des variables utilisées
 diametreRoue = 5.6 #cm
-circonferenceRoue = round(diametreRoue * math.pi, 2)
-
+circonferenceRoue = diametreRoue * math.pi
 direction = Direction.HAUT
-robot_x = None
-robot_y = None
-coords = None
-obstacle = None
-
-
-spkr.play_tone(400, 0.1, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
-coords = []
+coords = ""
 robot_x = 0
 robot_y = 0
+detecteUnObstacle = False
+
+DEBUG = False
+MAX_TIME = 15
+
+# Début du programme, bip sonore
+start_time = time.time()
+spkr.play_tone(400, 0.1, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
 time.sleep(500 / 1000)
-while not touch_sensor.is_pressed:
-    distance = round(ultrasonic_sensor.distance_centimeters)
-    robot_y += round(circonferenceRoue, 2)
-    tank_drive.on(SpeedRPS(1), SpeedRPS(1))
-    if distance < 255:
-        # Le capteur US a détecté quelque chose sur la droite du robot
-        if(direction == Direction.HAUT):
-            x = round(robot_x + distance, 2)
-            y = round(robot_y, 2)
-        else:
-            print("DIRECTION NON GÉRÉE: " + str(direction))
-        obstacle = [x, y]
-        print('Obstacle: ' + str(x) + ', ' + str(y))
-        coords.append(obstacle)
+
+# Le robot fait un tour sur lui-même pour repérer les obstacles proches
+# todo A DECOMMENTER
+# for i in range(4):
+#     tourne("droite")
+
+running = True
+while running:
+    # Calcul du temps écoulé
+    current_time = time.time()
+    temps_ecoule = (current_time - start_time)
+
+    if (not touch_sensor.is_pressed):
+        avance()
+    else:
+        # Heurte un obstacle
+        if (DEBUG):
+            print("Vient de heurter un obstacle")
+        recule()
+
+
+    # todo A COMMENTER - arrête le programme après 90 secondes
+    if (temps_ecoule >= MAX_TIME):
+        if (DEBUG):
+            print("Temps écoulé: ", str(round(temps_ecoule, 2)) + "s")
+        running = False
+
+
+
+# Fin du programme, bip sonore
 tank_drive.off(brake=True)
 spkr.play_tone(400, 0.1, play_type=Sound.PLAY_NO_WAIT_FOR_COMPLETE)
-print('Liste des coordonnées des obstacles' + str(coords))
+
+# Enregistrement des coordonnées
+print("Liste des coordonnées des obstacles:\n" + str(coords))
+#todo: enregistrer dans un fichier .txt
+# Ouvrir le fichier en mode écriture
+with open('environnement.txt', 'w') as file:
+    # for coord in coords:
+    #     # Écrire chaque coordonnée dans le fichier
+    #     file.write(str(coord[0]) + ',' + str(coord[1]) + '\n')
+    # Écrit les coordonnées dans le fichier texte
+    file.write(coords)
+# Donc là, on devrait avoir un fichier environnement.txt, avec des lignes type: "37.15, -204,17"
+
